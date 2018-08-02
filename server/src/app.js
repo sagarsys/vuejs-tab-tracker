1916e395eddc1726a506cbd3e4a67f346f76a722
sagar <sagar_sys@live.com>
2018-08-03 01:22:47 +0400

    -- basic server side implementation

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(morgan('combine'))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/status', (req, res) => {
   res.send({
     message: 'Hello World'
-  })
-})
+  });
+});
 
-app.listen(process.env.PORT || 9999)
+app.listen(process.env.PORT || 9999);
