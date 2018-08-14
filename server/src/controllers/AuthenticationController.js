8f99eb40e01b89dcbb0bbc0feda75e0e277eb97d
sagar <sagar_sys@live.com>
2018-08-14 16:21:36 +0400

    - add login route & controller

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -11,5 +11,35 @@ module.exports = {
 				error: 'This email account is already in use.'
 			})
 		}
+	},
+	async login(req, res) {
+		try {
+			const {email, password} = req.body;
+
+			const user = await User.findOne({
+				where: { email: email }
+			});
+			if (!user) {
+				return res.status(403).send({
+					error: 'Invalid email or password'
+				})
+			}
+
+			const isPassValid = password === user.password
+			if (!isPassValid) {
+				return res.status(403).send({
+					error: 'Invalid email or password'
+				})
+			}
+
+			res.send({
+				user: user.toJSON()
+			})
+
+		} catch (e) {
+			res.status(500).send({
+				error: 'An unexpected error occurred.'
+			})
+		}
 	}
 };
