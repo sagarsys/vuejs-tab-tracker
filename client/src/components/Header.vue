031759f2d67b2464dd923306bdff621920689f82
sagar <sagar_sys@live.com>
2018-08-17 17:41:22 +0400

    - implement logout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -4,13 +4,16 @@
         <v-btn large flat cyan to="/">Tab Tracker</v-btn>
       </v-toolbar-title>
       <!-- TODO: Implement main menu -->
-      <!--<v-toolbar-items>-->
-        <!--<v-btn flat>Browse</v-btn>-->
-      <!--</v-toolbar-items>-->
+      <v-toolbar-items v-if="$store.state.isUserLoggedIn">
+        <v-btn flat>Browse</v-btn>
+      </v-toolbar-items>
       <v-spacer></v-spacer>
       <v-toolbar-items v-if="!$store.state.isUserLoggedIn">
         <v-btn flat @click="navigateTo({name: 'login'})">Sign In</v-btn>
         <v-btn flat @click="navigateTo({name: 'register'})">Sign Up</v-btn>
+      </v-toolbar-items>
+      <v-toolbar-items v-else>
+        <v-btn flat @click="logout">Sign Out</v-btn>
       </v-toolbar-items>
     </v-toolbar>
 </template>
@@ -20,7 +23,12 @@
 		name: 'AppHeader',
     methods: {
 		  navigateTo(route) {
-		    this.$router.push(route);
+		    this.$router.push(route)
+      },
+      logout() {
+		    this.$store.dispatch('setToken', null)
+		    this.$store.dispatch('setUser', null)
+        this.$router.push({ name: 'root' })
       }
     }
 	}
